caf1d2ebc4af54f41dea9ab4fa72b7cbcf153c54
talha206 <ctalha512@gmail.com>
2024-11-20 20:45:55 +0500

    Font size issues done for sm screens

--- app/tickets/Ticket.tsx
+++ app/tickets/Ticket.tsx
@@ -10,16 +10,16 @@ interface TicketTimeTrackerProps {
 const TicketTimeTracker: React.FC<TicketTimeTrackerProps> = ({ tickets }) => {
   
   return (
-    <div className=" font-mono min-h-screen">
+    <div className=" font-mono h-auto">
 
-      <div className="bg-white rounded-lg  ">
+      <div className="bg-white rounded-lg w-max ">
         {/* Header Row */}
-        <div className="grid grid-cols-2 sm:gap-4 font-semibold   pb-2 mb-2">
-          <div className='2xl:text-3xl text-[10px] sm:text-sm md:text-md lg:text-lg xl:text-xl'>Ticket</div>
-          <div className='2xl:text-3xl text-[10px] sm:text-sm md:text-md lg:text-lg xl:text-xl' >Time Logged</div>
+        <div className="grid sm:grid-cols-[300px,250px] grid-cols-[200px,150px] gap-x-4 font-normal pb-2 mb-2">
+          <div className='2xl:text-md text-[10px] sm:text-sm italic '>Ticket</div>
+          <div className='2xl:text-md text-[10px] sm:text-sm italic text-[#000000b0]'  >Time Logged</div>
         </div>
-           <span className='absolute  xl:left-[390px] xl:top-[319px] lg:left-[310px] lg:top-[325px] 2xl:left-[825px] 2xl:top-[360px] md:top-[618px] md:left-[48%] sm:left-[48%] left-[54%]'>
-             <svg  className=' md:w-[3px] 2xl:h-[380px]  xl:h-[289px] lg:h-[240px] md:h-[244px] sm:h-[225px] sm:w-[6px] h-[160px] w-[2px] ' viewBox="0 0 2 226" fill="none" xmlns="http://www.w3.org/2000/svg">
+           <span className='absolute  xl:left-[390px] xl:top-[257px] lg:left-[390px] lg:top-[255px] 2xl:left-[825px] 2xl:top-[360px] md:top-[572px] md:left-[56%] sm:left-[57%] left-[54%]'>
+             <svg  className=' md:w-[3px] 2xl:h-[380px]  xl:h-[279px] lg:h-[240px] md:h-[244px] sm:h-[240px] sm:w-[6px] h-[160px] w-[2px] ' viewBox="0 0 2 226" fill="none" xmlns="http://www.w3.org/2000/svg">
               <line opacity="0.6" x1="1.23338" y1="0.443604" x2="1.23337" y2="285.451" stroke="url(#paint0_linear_3_12)" strokeOpacity="0.6" strokeWidth="0.896445"/>
               <defs>
               <linearGradient id="paint0_linear_3_12" x1="0.285156" y1="0.443603" x2="0.285146" y2="225.451" gradientUnits="userSpaceOnUse">
@@ -32,9 +32,9 @@ const TicketTimeTracker: React.FC<TicketTimeTrackerProps> = ({ tickets }) => {
   <div className='2xl:mt-8'>
         {/* Ticket Rows */}
         {tickets.map((ticket) => (
-          <div key={ticket.id} className="grid grid-cols-2 sm:gap-4 gap-0 items-center gradient-border-b  xl:py-4 sm:py-3 py-1  ">
+          <div key={ticket.id} className="grid sm:grid-cols-[300px,250px] grid-cols-[230px,130px]  sm:gap-x-4 gap-x-1 items-center gradient-border-b  xl:py-4 sm:py-3 py-1  ">
             {/* Left Column - Ticket Info */}
-            <div className="flex items-center pr-6">
+            <div className="flex items-center sm:pr-6   justify-normal">
                 
               <span className="mr-2">
                 <svg  className='xl:w-[23px] lg:w-[18px] md:w-[19px] sm:w-[17px] w-[10px] ' viewBox="0 0 23 22" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -42,13 +42,13 @@ const TicketTimeTracker: React.FC<TicketTimeTrackerProps> = ({ tickets }) => {
                     <circle cx="11.5501" cy="11.0618" r="4.48223" fill="white"/>
                  </svg>
                </span>
-              <span className="font-medium  2xl:text-3xl lg:text-[9px] xl:text-[12px] md:text-[13px] sm:text-[11px] text-[4px]">{ticket.id}</span>
-              <span className="ml-2 text-black font-normal truncate md:text-[13px] sm:text-[11px] 2xl:text-3xl lg:text-[10px] xl:text-sm text-[4px]">{ticket.title}</span>
+              <span className="font-medium  md:text-[13px] sm:text-[12px] text-[9px] whitespace-nowrap ">{ticket.id}</span>
+              <span className="ml-2 text-black font-normal  md:text-[13px] sm:text-[12px] text-[9px]  whitespace-nowrap">{ticket.title}</span>
             </div>
 
             {/* Right Column - Time Logged */}
             <div >
-              <span className='font-normal 2xl:text-3xl lg:text-[10px] xl:text-sm sm:text-[11px] md:text-[13px] text-[6px] '>{ticket.timeLogged}</span>
+              <span className='font-normal 2xl:text-3xl md:text-[13px] sm:text-[12px] text-[9px] sm:pl-8 '>{ticket.timeLogged}</span>
                
             </div>
           </div>
@@ -60,3 +60,10 @@ const TicketTimeTracker: React.FC<TicketTimeTrackerProps> = ({ tickets }) => {
 };
 
 export default TicketTimeTracker;
+
+
+// 2xl:text-3xl lg:text-[9px] xl:text-[12px] 
+
+
+//2xl:text-3xl lg:text-[10px] xl:text-sm 
+
--- app/tickets/page.tsx
+++ app/tickets/page.tsx
@@ -4,20 +4,20 @@ import { tickets } from '../../lib/tickets';
 
 const Ticket = () => {
   return (
-    <div className='w-[100%] font-mono'>
-      <h1 className='w-[100%] 2xl:text-6xl lg:text-4xl md:text-2xl  sm:text-lg text-sm py-6 px-16 text-slate-500 d-flex flex-1 font-mono bg-zinc-600 tracking-widest'>Ticket Time Tracker</h1>
+    <div className=' w-[100%] h-auto font-mono'>
+      <h1 className='w-50 sm:w-[100%] 2xl:text-3xl sm:text-2xl text-lg py-6 sm:pl-16  pl-8  text-[#9B9BC3] d-flex flex-1 font-mono font-medium bg-[#424247] tracking-widest'>Ticket Time Tracker</h1>
       <div className='2xl:px-40 lg:px-14 md:px-12 sm:px-8 py-10 '>
          
-          <div className='flex flex-col-reverse lg:flex-row  items-center justify-between font-mono'>
+          <div className='flex flex-col-reverse lg:flex-row  sm:items-center justify-between   font-mono'>
           
-          <div >
-          <h2 className=' 2xl:text-5xl lg:text-3xl  md:text-xl text-lg font-medium sm:mb-50 mt-6 ml-10 sm:ml-0'>Existing tickets</h2>
-          <div className='pt-7 sm:pt-12  ml-14 sm:ml-0'>
+          <div className='inline-block'>
+          <h2 className=' 2xl:text-2xl lg:text-xl  md:text-lg text-sm font-medium sm:mb-50 mt-6 ml-10 sm:ml-0'>Existing tickets</h2>
+          <div className='pt-7 sm:pt-12  ml-9 sm:ml-0'>
           
           <TicketTimeTracker tickets={tickets} />
           </div>
           </div>
-            <div className='bg-[#EFEFF0]  rounded-xl lg:w-[40%] xl:w-[40%] 2xl:w-[35%] w-[60%]  2xl:h-[44rem] sm:h-80 h-60 md:text-center lg:h-[25rem] lg:mt-20 xl:h-[30rem] xl:mt-0  lg:px-12 md:px-10 px-8 py-8 '>
+            <div className='bg-[#EFEFF0]  rounded-xl lg:w-[40%] xl:w-[40%] 2xl:w-[35%] w-[60%]  2xl:h-[44rem] sm:h-80 h-60 md:text-center lg:h-[25rem] lg:mt-20 xl:h-[30rem] xl:mt-0 sm:ml-0 ml-20 lg:px-12 md:px-10 px-8 py-8 '>
               <h1 className='xl:text-lg lg:text-sm  sm:text-[13px] text-[10px] text-center 2xl:text-3xl '> Select an existing ticket to log time <br /> or <br /><span className='underline underline-offset-2'>Create a new one</span> </h1> 
               <div className='xl:pt-20 md:pt-8 pt-6'>
                      <span className='mr-2 bg-[#D9D9D9] 2xl:h-16 xl:h-10 lg:h-8 md:h-6 h-4 w-[100%]  inline-block'></span>
